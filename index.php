400802f7317fddd98baba91aaee837772af57477
Andres Arias <aarias@iluv2code.com>
2015-09-07 14:54:19 -0600

    Updated $system_path in index.php, so it points to a shared system directory of CodeIgniter 3.0.1

--- index.php
+++ index.php
@@ -98,7 +98,9 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'system';
+
+ //AArias: change this to the proper system path in your environment
+	$system_path = '../CodeIgniter-3.0.1/system';
 
 /*
  *---------------------------------------------------------------
